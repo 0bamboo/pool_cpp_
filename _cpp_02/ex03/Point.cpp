11c8e5b176c5ffa0f62bcbc829b5c3f4511f8de1
nowl01 <itsabdennacerama@gmail.com>
2021-11-15 22:00:50 +0100

    ll

--- _cpp_02/ex03/Point.cpp
+++ _cpp_02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 18:22:13 by abdait-m          #+#    #+#             */
-/*   Updated: 2021/11/15 21:13:57 by abdait-m         ###   ########.fr       */
+/*   Updated: 2021/11/15 21:33:43 by abdait-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ Point::Point():x(0), y(0) {}
 
 Point::Point(const float f1, const float f2):x(f1) , y(f2) {}
 
-Point::Point(const Point& copy)
-{
-	*this = copy;
-}
+Point::Point(const Point& copy): x(copy.x) , y(copy.y) {}
 
 Fixed const&	Point::_get_x_() const
 {
@@ -28,7 +25,7 @@ Fixed const&	Point::_get_x_() const
 
 Point&			Point::operator=(const Point& copy)
 {
-	copy.~Point();
+	(void)copy;
 	return (*this);
 }
 
--- _cpp_02/ex03/main.cpp
+++ _cpp_02/ex03/main.cpp
@@ -6,17 +6,22 @@
 /*   By: abdait-m <abdait-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 20:36:15 by abdait-m          #+#    #+#             */
-/*   Updated: 2021/11/15 21:19:41 by abdait-m         ###   ########.fr       */
+/*   Updated: 2021/11/15 21:59:58 by abdait-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-float	_calc_area_(Point const& px, Point const& py, Point const& pz)
+float	_calc_area_(Point const& p1, Point const& p2, Point const& p3)
 {
-	return ((px._get_x_().toFloat() * (py._get_y_().toFloat() - pz._get_y_().toFloat()) \
-			+ (py._get_x_().toFloat() * (pz._get_y_().toFloat() - px._get_y_().toFloat())) \
-			+ (pz._get_x_().toFloat() * (px._get_y_().toFloat() - py._get_y_().toFloat()))) / 2);
+	float var1 = p1._get_x_().toFloat() * (p2._get_y_().toFloat() - p3._get_y_().toFloat());
+	float var2 = p2._get_x_().toFloat() * (p3._get_y_().toFloat() - p1._get_y_().toFloat());
+	float var3 = p3._get_x_().toFloat() * (p1._get_y_().toFloat() - p2._get_y_().toFloat());
+	
+	std::cout << p1._get_x_().toFloat() << "|" << p2._get_y_().toFloat() << "|" << p3._get_y_().toFloat() << "|" << var1 << std::endl;
+	std::cout << p2._get_x_().toFloat() << "|" << p3._get_y_().toFloat() << "|" << p1._get_y_().toFloat()<< "|" << var2 << std::endl;
+	std::cout << p3._get_x_().toFloat() << "|" << p1._get_y_().toFloat() << "|" << p2._get_y_().toFloat() << "|" << var3 << std::endl;
+	return (std::fabs((var1 + var2 + var3) / 2));
 }
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point) 
@@ -25,21 +30,21 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 	// float	pab_area = _calc_area_(point, a, b);
 	// float	pbc_area = _calc_area_(point, b, c);
 	// float	pac_area = _calc_area_(point, a, c);
-	
-	std::cout << a._get_x_().toFloat() << std::endl;
+	(void)point;
 	std::cout << abc_area << std::endl;
-	point.~Point();
 	return true;
 }
 
 int main( void )
 {
-	Point a(1, 1);
-	Point b(4, 2);
-	Point c(3, 5);
-	Point point(2.33, 2);
+	Point a(0,0);
+	Point b(20, 0);
+	Point c(10, 30);
+	Point point(10, 15);
 
-	std::cout << a._get_x_().toFloat() << std::endl;
+	std::cout << a._get_x_().toFloat() << "--" << a._get_y_().toFloat() << std::endl;
+	std::cout << b._get_x_().toFloat() << "--" << b._get_y_().toFloat() << std::endl;
+	std::cout << c._get_x_().toFloat() << "--" << c._get_y_().toFloat() << std::endl;
 	bsp(a, b, c, point);
 	return 0;
 }
